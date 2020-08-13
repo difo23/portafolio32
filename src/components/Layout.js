2379a8b007b550cdb598204300a52148df0fcb01
difo23 <diffozanzan@gmail.com>
2020-08-13 01:05:46 -0400

    add helmet tiitle

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,19 @@
 import React from "react"
 // import "../css/main.css"
+import { Helmet } from "react-helmet";
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
+
+
 const Layout = ({ children }) => {
   return (
     <>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>Lizandro's portfolio</title>
+          <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet>
       <Navbar/>
       {children}
     </>
