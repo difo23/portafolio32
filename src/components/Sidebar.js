b88bca8d5ef80c45f6b9340cbb1216c6add1af9f
difo23 <diffozanzan@gmail.com>
2020-08-17 16:25:09 -0400

    Sidebar uptdate

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react"
 import Links from "../constants/links"
 import SocialLinks from "../constants/socialLinks"
 import { FaTimes } from "react-icons/fa"
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
     <aside className={`sidebar ${isOpen ? "show-sidebar" : ""} `}>
       <button className="close-btn" onClick={toggleSidebar}>
