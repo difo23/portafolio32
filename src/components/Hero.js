11beec2e885db80b13bda25a74ddb0bb9b7ae049
difo23 <diffozanzan@gmail.com>
2021-01-03 21:12:46 -0400

    delete iam

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -23,7 +23,7 @@ const Hero = () => {
 				<article className="hero-info">
 					<div>
 						<div className="underline" />
-						<h1>I'm Lizandro J. Ramírez</h1>
+						<h1>Lizandro J. Ramírez</h1>
 						<h4>Software engineer</h4>
 						<a href="https://www.linkedin.com/in/lizandro23difo" className="btn">
 							Contact me
