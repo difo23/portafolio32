175107ef84b1b44011ab0b64b0263c7cafa1a3c7
difo23 <diffozanzan@gmail.com>
2021-01-06 20:38:23 -0400

    Text role

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,18 +5,18 @@ export default [
 		id: 1,
 		icon: <FaCode className="service-icon" />,
 		title: 'Software Engineer',
-		text: `I have master degree in software for distributed and embedded system. I like to work with  internet of things like microcontrollers, FPGA and VHDL. During my master I was worked with Apache distributed systems like Hadoop, Zookeeper, Storm and spark.`
+		text: `Master degree in software for distributed and embedded system. I like to work with  internet of things like microcontrollers, FPGA and VHDL. During my master I was worked with Apache distributed systems like Hadoop, Zookeeper, Storm and spark.`
 	},
 	{
 		id: 2,
 		icon: <FaStackOverflow className="service-icon" />,
 		title: 'FullStack Web Development',
-		text: `I'm current working with Mongodb,  Express, React and Nodejs (MERN) stack technologies in web. I am fanboy of vanilla JS. I don't have problem with to use others programming languages about the backend like are Python, Go and Java.`
+		text: `Current working with Mongodb,  Express, React and Nodejs (MERN) stack technologies in web. I am fanboy of vanilla JS. I don't have problem with to use others programming languages about the backend like are Python, Go and Java.`
 	},
 	{
 		id: 3,
 		icon: <FaNetworkWired className="service-icon" />,
 		title: 'Networking Engineer',
-		text: `I am telematics engineer, I have passion with electronics devices and the networks of telecommunications, this background let me know better how to work with the software in this embedded devices.`
+		text: `Telematics engineer, passion by electronics devices and the networks, this background let me know better how to work with the software in this embedded devices.`
 	}
 ];
